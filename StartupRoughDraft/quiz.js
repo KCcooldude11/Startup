3aae3f39f113f63e01848d0e72a492de6582275a
KCcooldude11 <112493584+KCcooldude11@users.noreply.github.com>
2023-03-06 13:06:15 -0700

    changed main.html to index.html

---
+++ StartupRoughDraft/quiz.js
@@ -0,0 +1,7 @@
+const quizContainer = document.getElementById('quiz');
+const resulstContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+function buildQuiz(){}
+function showResults(){}
+
+submitButton.Button.addEventListener('click', showResults);
