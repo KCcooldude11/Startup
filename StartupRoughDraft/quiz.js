53845d5c9444369e1a61bac6216d92b2ba35997e
KCcooldude11 <112493584+KCcooldude11@users.noreply.github.com>
2023-03-10 15:14:25 -0700

    lots of changes to the quiz!

--- StartupRoughDraft/quiz.js
+++
@@ -1,7 +0,0 @@
-const quizContainer = document.getElementById('quiz');
-const resulstContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-function buildQuiz(){}
-function showResults(){}
-
-submitButton.Button.addEventListener('click', showResults);
