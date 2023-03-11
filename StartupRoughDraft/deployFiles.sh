c5dce049fe2b98a0dbb85756e9a5f1c20520e980
KCcooldude11 <112493584+KCcooldude11@users.noreply.github.com>
2023-03-10 17:07:30 -0700

    iroh name change

--- StartupRoughDraft/deployFiles.sh
+++ StartupRoughDraft/deployFiles.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 while getopts k:h:s: flag
 do
     case "${flag}" in
@@ -19,11 +17,11 @@ printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i $key ubuntu@$hostname << ENDSSH
+ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i $key * ubuntu@$hostname:services/$service/public
+scp -r -i "$key" * ubuntu@$hostname:services/$service/public
